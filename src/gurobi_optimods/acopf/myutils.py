b4f1d610528b07a69a5477a4c11388d2caaf6402
Jaromil Najman <najman@gurobi.com>
2022-12-09 17:11:32 +0100

    Additional comment on breakexit

--- src/gurobi_optimods/acopf/myutils.py
+++ src/gurobi_optimods/acopf/myutils.py
@@ -2,6 +2,7 @@ import sys
 
 
 def breakexit(foo):
+    """Will be removed in the final version"""
     stuff = input("("+foo+") break> ")
     if stuff == 'x' or stuff == 'q':
         sys.exit("bye")
