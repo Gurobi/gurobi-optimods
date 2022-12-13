38a1988e4dee9ad6ad20478569ad44239ac8a6e9
Simon Bowly <bowly@gurobi.com>
2022-12-13 23:35:10 +1100

    Add unit test

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,8 +1,15 @@
 import unittest
 
+import numpy as np
+
 from gurobi_optimods.regression import L1Regression
 
 
 class TestL1Regression(unittest.TestCase):
-    def test_something(self):
-        pass
+    def test_random(self):
+        reg = L1Regression()
+        X_train = np.random.random((100, 5))
+        y_train = np.random.random((100))
+        reg.fit(X_train, y_train)
+        y_pred = reg.predict(np.random.random((30, 5)))
+        self.assertEqual(y_pred.shape, (30,))
