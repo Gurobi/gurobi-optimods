735e655ab6967345878467cd0241f1dba47b4a8f
Simon Bowly <bowly@gurobi.com>
2023-01-04 08:56:58 +1100

    Add gurobi_optimods.datasets module
    Fixes #5. Remove old feather datafiles in favour of csv files
    and importable load() functions for each mod.

---
+++ src/gurobi_optimods/datasets.py
@@ -0,0 +1,32 @@
+"""
+Module for loading datasets for use in optimods examples, in the same vein
+as sklearn.datasets.
+"""
+
+import pathlib
+
+import pandas as pd
+
+DATA_FILE_DIR = pathlib.Path(__file__).parent / "data"
+
+
+class AttrDict(dict):
+    """Even simpler version of sklearn's Bunch"""
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+
+def load_workforce():
+    return AttrDict(
+        availability=pd.read_csv(
+            DATA_FILE_DIR / "availability.csv", parse_dates=["Shift"]
+        ),
+        pay_rates=pd.read_csv(DATA_FILE_DIR / "pay_rates.csv"),
+        shift_requirements=pd.read_csv(
+            DATA_FILE_DIR / "shift_requirements.csv", parse_dates=["Shift"]
+        ),
+    )
--- tests/test_workforce.py
+++ tests/test_workforce.py
@@ -1,8 +1,26 @@
 import unittest
 
+from pandas.api.types import is_object_dtype, is_datetime64_any_dtype, is_numeric_dtype
+
 from gurobi_optimods.workforce import solve_workforce_scheduling
+from gurobi_optimods.datasets import load_workforce
 
 
 class TestWorkforceScheduling(unittest.TestCase):
-    def test_something(self):
-        pass
+    def test_dataset(self):
+        data = load_workforce()
+        self.assertEqual(
+            set(data.keys()), {"availability", "pay_rates", "shift_requirements"}
+        )
+
+        self.assertEqual(set(data.availability.columns), {"Worker", "Shift"})
+        self.assertTrue(is_object_dtype(data.availability["Worker"]))
+        self.assertTrue(is_datetime64_any_dtype(data.availability["Shift"]))
+
+        self.assertEqual(set(data.pay_rates.columns), {"Worker", "PayRate"})
+        self.assertTrue(is_object_dtype(data.pay_rates["Worker"]))
+        self.assertTrue(is_numeric_dtype(data.pay_rates["PayRate"]))
+
+        self.assertEqual(set(data.shift_requirements.columns), {"Shift", "Required"})
+        self.assertTrue(is_datetime64_any_dtype(data.shift_requirements["Shift"]))
+        self.assertTrue(is_numeric_dtype(data.shift_requirements["Required"]))
