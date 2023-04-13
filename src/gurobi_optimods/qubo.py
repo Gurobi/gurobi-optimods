92ff91c4c3e5405aff021d15ca8a1bd52142196c
Mario Ruthmair <ruthmair@gurobi.com>
2023-04-13 16:05:30 +0200

    basic tests + implementation

--- src/gurobi_optimods/qubo.py
+++ src/gurobi_optimods/qubo.py
@@ -6,17 +6,36 @@
 # whatever makes the most sense for this mod.
 
 import gurobipy as gp
+from gurobipy import GRB
+import numpy as np
 
 
-def solve_mod(data):
+def solve_qubo(coeff_matrix):
     """
     A sphinx-compatible docstring
 
     :param data1: Data structure for first argument
     :type data1: pd.DataFrame
     """
+
+    if coeff_matrix is None:
+        return None
+
+    # TODO return useful error
+    if coeff_matrix.ndim != 2:
+        return None
+
+    shape = coeff_matrix.shape
+    if shape[0] != shape[1]:
+        return None
+
+    n = shape[0]
+
     with gp.Env() as env, gp.Model(env=env) as model:
-        # build model
+
+        x = model.addMVar(n, vtype=GRB.BINARY)
+        model.setObjective(x @ coeff_matrix @ x, GRB.MINIMIZE)
+
         model.optimize()
-        # post-process and return solution
-        return
+
+        return x.X.round()
--- tests/test_qubo.py
+++ tests/test_qubo.py
@@ -3,18 +3,29 @@
 # This template should be copied to tests/test_<mod-name>.py
 
 import unittest
+import numpy as np
 
-from gurobi_optimods.datasets import load_mod_example_data
-from gurobi_optimods.mod import solve_mod
+# from gurobi_optimods.datasets import load_mod_example_data
+from gurobi_optimods.qubo import solve_qubo
 
 
-class TestMod(unittest.TestCase):
-    def test_datasets(self):
-        # If you added something to optimods.datasets, test it here
-        data = load_mod_example_data()
-        self.assertEqual(set(data.keys()), {"a", "b", "c"})
+class TestQubo(unittest.TestCase):
+    # def test_datasets(self):
+    #     # If you added something to optimods.datasets, test it here
+    #     data = load_mod_example_data()
+    #     self.assertEqual(set(data.keys()), {"a", "b", "c"})
 
-    def test_simple(self):
-        data = None
-        solution = solve_mod(data)
+    def test_none(self):
+        Q = None
+        solution = solve_qubo(coeff_matrix=Q)
         self.assertTrue(solution is None)
+
+    def test_positive(self):
+        Q = np.array([[2, 5], [3, 6]])
+        solution = solve_qubo(coeff_matrix=Q)
+        self.assertEqual(solution, np.array([0, 0]))
+
+    def test_negative(self):
+        Q = np.array([[-2, -5], [-3, -6]])
+        solution = solve_qubo(coeff_matrix=Q)
+        self.assertEqual(solution, np.array([1, 1]))
