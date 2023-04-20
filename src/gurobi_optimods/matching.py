3d0590813654aca266bb25fc6a7dbc7101e84450
Simon Bowly <bowly@gurobi.com>
2023-04-20 10:39:15 +1000

    Add network flow matching formulation

--- src/gurobi_optimods/matching.py
+++ src/gurobi_optimods/matching.py
@@ -57,6 +57,70 @@ def maximum_bipartite_matching(G, *, create_env):
         return sp.coo_array((data, (i, j)), shape=G.shape)
 
 
+def maximum_bipartite_matching_flow(adjacency, nodes1, nodes2):
+
+    G_nodes = adjacency.shape[0]
+    source, sink = G_nodes, G_nodes + 1
+    N_nodes = G_nodes + 2
+    G = sp.triu(adjacency.tocoo())
+
+    logger.info(
+        f"Solving maximum matching n1={nodes1.shape[0]} "
+        f"n2={nodes2.shape[0]} |E|={G.data.shape[0]}"
+    )
+
+    # Build network:
+    #   source -> nodes1 (complete)
+    #   nodes1 -> nodes2 (adjacency)
+    #   nodes2 -> sink (complete)
+    #   sink -> source
+    from_arc = np.concatenate([[source] * nodes1.shape[0], G.row, nodes2, [sink]])
+    to_arc = np.concatenate([nodes1, G.col, [sink] * nodes2.shape[0], [source]])
+    logger.info(
+        "Maximum matching formulated as min-cost flow "
+        f"with {N_nodes} nodes and {from_arc.shape[0]} arcs"
+    )
+
+    # Formulate as min-cost flow (A, b, c, u)
+    #
+    #   min  c^T x
+    #  s.t.  A x = b
+    #        0 <= x <= u
+    #
+    s = from_arc.shape[0]
+    j = np.concatenate([np.arange(s), np.arange(s)])
+    i = np.concatenate([from_arc, to_arc])
+    data = np.concatenate([np.ones(s), -1 * np.ones(s)])
+    A = sp.coo_matrix((data, (i, j)))
+    u = np.ones(s)
+    u[-1] = GRB.INFINITY
+    c = np.zeros(s)
+    c[-1] = -1
+    names = [f"({i},{j})" for i, j in zip(from_arc, to_arc)]
+    b = np.zeros(N_nodes)
+
+    # Solve flow model with gurobi
+    with gp.Env() as env, gp.Model(env=env) as m:
+        m.ModelSense = GRB.MINIMIZE
+        x = m.addMVar(s, lb=0, ub=u, obj=c, name=names)
+        m.addMConstr(A, x, GRB.EQUAL, b)
+        m.optimize()
+        flows = x.X
+
+    # Choose the arcs corresponding to the original graph with non-zero
+    # flow. Note that the last var is the sink->source connection (drop it).
+    select = (flows > 0.5) & (from_arc != source) & (to_arc != sink)
+    from_arc_result = from_arc[select][:-1]
+    to_arc_result = to_arc[select][:-1]
+
+    logger.info(f"Maximum bipartite matching has {from_arc_result.shape[0]} edges")
+
+    # Return undirected, unweighted adjacency matrix
+    arg = (np.ones(from_arc_result.shape), (from_arc_result, to_arc_result))
+    matching = sp.coo_matrix(arg, dtype=float, shape=G.shape)
+    return matching + matching.T
+
+
 @optimod(mod_logger=logger)
 def maximum_weighted_matching(G, *, create_env):
     """Return a subgraph which is the maximum weighted matching of G.
