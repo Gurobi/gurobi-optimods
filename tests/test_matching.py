97bcf8ba9b57b253ab3965530dba419c51c2df55
Simon Bowly <bowly@gurobi.com>
2023-04-20 10:38:57 +1000

    Add bipartite matching tests

--- tests/test_matching.py
+++ tests/test_matching.py
@@ -1,12 +1,14 @@
 import unittest
+from itertools import chain, product
 
 import networkx as nx
 import numpy as np
 import scipy.sparse as sp
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_allclose, assert_array_equal
 
 from gurobi_optimods.matching import (
     maximum_bipartite_matching,
+    maximum_bipartite_matching_flow,
     maximum_weighted_matching,
 )
 
@@ -39,6 +41,82 @@ class TestBipartiteMatching(unittest.TestCase):
         G = sp.coo_array((data, (row, col)), shape=(3, 3))
         with self.assertRaises(ValueError):
             maximum_bipartite_matching(G)
+
+
+class TestBipartiteMatchingFlow(unittest.TestCase):
+    def assert_is_unweighted_matching(self, matching):
+        assert_allclose(matching.data, np.ones(matching.data.shape))
+        adj = matching.todense()
+        assert_allclose(adj, adj.T)
+        self.assertTrue(np.alltrue(adj.sum(axis=0) <= 1))
+
+    def test_empty(self):
+        # Matching of an empty graph is empty
+        nodes1 = np.arange(5)
+        nodes2 = np.arange(5, 13)
+        degree = nodes1.shape[0] + nodes2.shape[0]
+        adjacency = sp.coo_matrix(([], ([], [])), shape=(degree, degree))
+
+        matching = maximum_bipartite_matching_flow(adjacency, nodes1, nodes2)
+
+        self.assertIsInstance(matching, sp.spmatrix)
+        self.assertIsNot(matching, adjacency)
+        self.assertEqual(matching.nnz, 0)
+        self.assertEqual(matching.shape, (degree, degree))
+        self.assert_is_unweighted_matching(matching)
+
+    def test_complete_bipartite(self):
+        # Matching of a complete graph has min(n1, n2) edges, and is a matching
+        n1, n2 = 6, 8
+        nodes1 = np.arange(n1)
+        nodes2 = np.arange(n1, n1 + n2)
+        degree = nodes1.shape[0] + nodes2.shape[0]
+        edges = list(chain(product(nodes1, nodes2), product(nodes2, nodes1)))
+        i, j = zip(*edges)
+        data = np.ones(len(i))
+        adjacency = sp.coo_matrix((data, (i, j)), shape=(degree, degree))
+        expect_matching_size = min(n1, n2)
+
+        matching = maximum_bipartite_matching_flow(adjacency, nodes1, nodes2)
+
+        self.assertIsInstance(matching, sp.spmatrix)
+        self.assertIsNot(matching, adjacency)
+        self.assertEqual(matching.shape, (degree, degree))
+        self.assert_is_unweighted_matching(matching)
+        self.assertEqual(matching.nnz, expect_matching_size * 2)  # symmetric
+
+    def test_simple(self):
+        nodes1 = np.array([1, 3, 4, 6])
+        nodes2 = np.array([0, 2, 5])
+        degree = nodes1.shape[0] + nodes2.shape[0]
+        i = np.array([1, 3, 4, 6])
+        j = np.array([2, 5, 5, 2])
+        data = np.ones(4)
+        triu = sp.coo_matrix((data, (i, j)), shape=(degree, degree))
+        adjacency = triu + triu.T
+        expect_matching_size = 2
+
+        matching = maximum_bipartite_matching_flow(adjacency, nodes1, nodes2)
+
+        self.assertIsInstance(matching, sp.spmatrix)
+        self.assertIsNot(matching, adjacency)
+        self.assertEqual(matching.shape, (degree, degree))
+        self.assert_is_unweighted_matching(matching)
+        self.assertEqual(matching.nnz, expect_matching_size * 2)  # symmetric
+
+    def test_networkx_random(self):
+        n1, n2, p, seed = 6, 8, 0.5, 2394
+        nodes1 = np.arange(n1)
+        nodes2 = np.arange(n1, n1 + n2)
+        graph = nx.bipartite.random_graph(n1, n2, p, seed, directed=False)
+        adjacency = nx.to_scipy_sparse_array(graph)
+
+        matching = maximum_bipartite_matching_flow(adjacency, nodes1, nodes2)
+
+        self.assertIsInstance(matching, sp.spmatrix)
+        self.assertIsNot(matching, adjacency)
+        self.assertEqual(matching.shape, adjacency.shape)
+        self.assert_is_unweighted_matching(matching)
 
 
 class TestWeightedMatching(unittest.TestCase):
