c36fa98d27336bd85bfe727a32b322fa320cb212
Simon Bowly <bowly@gurobi.com>
2023-04-20 12:50:03 +1000

    Separate min-cost flow solver

--- src/gurobi_optimods/matching.py
+++ src/gurobi_optimods/matching.py
@@ -57,8 +57,34 @@ def maximum_bipartite_matching(G, *, create_env):
         return sp.coo_array((data, (i, j)), shape=G.shape)
 
 
-def maximum_bipartite_matching_flow(adjacency, nodes1, nodes2):
+def solve_min_cost_flow(env, from_arc, to_arc, capacity, cost):
+    # Formulate as min-cost flow (A, b, c, u)
+    #
+    #   min  c^T x
+    #  s.t.  A x = b
+    #        0 <= x <= u
+    #
+    s = from_arc.shape[0]
+    i = np.concatenate([from_arc, to_arc])
+    j = np.concatenate([np.arange(s), np.arange(s)])
+    data = np.ones(s * 2, dtype=float)
+    data[s:] *= -1.0
+    A = sp.coo_matrix((data, (i, j))).tocsr()
+    b = np.zeros(A.shape[0])
+    u = capacity
+    c = cost
+    names = np.array([f"x[{i},{j}]" for i, j in zip(from_arc, to_arc)])
 
+    # Solve model with gurobi, return flows
+    with gp.Model(env=env) as m:
+        m.ModelSense = GRB.MINIMIZE
+        x = m.addMVar(s, lb=0, ub=u, obj=c, name=names)
+        m.addMConstr(A, x, GRB.EQUAL, b)
+        m.optimize()
+        return x.X
+
+
+def maximum_bipartite_matching_flow(adjacency, nodes1, nodes2):
     G_nodes = adjacency.shape[0]
     source, sink = G_nodes, G_nodes + 1
     N_nodes = G_nodes + 2
@@ -76,36 +102,18 @@ def maximum_bipartite_matching_flow(adjacency, nodes1, nodes2):
     #   sink -> source
     from_arc = np.concatenate([[source] * nodes1.shape[0], G.row, nodes2, [sink]])
     to_arc = np.concatenate([nodes1, G.col, [sink] * nodes2.shape[0], [source]])
+    capacity = np.ones(from_arc.shape, dtype=float)
+    capacity[-1] = GRB.INFINITY
+    cost = np.zeros(from_arc.shape, dtype=float)
+    cost[-1] = -1.0
     logger.info(
         "Maximum matching formulated as min-cost flow "
         f"with {N_nodes} nodes and {from_arc.shape[0]} arcs"
     )
 
-    # Formulate as min-cost flow (A, b, c, u)
-    #
-    #   min  c^T x
-    #  s.t.  A x = b
-    #        0 <= x <= u
-    #
-    s = from_arc.shape[0]
-    j = np.concatenate([np.arange(s), np.arange(s)])
-    i = np.concatenate([from_arc, to_arc])
-    data = np.concatenate([np.ones(s), -1 * np.ones(s)])
-    A = sp.coo_matrix((data, (i, j)))
-    u = np.ones(s)
-    u[-1] = GRB.INFINITY
-    c = np.zeros(s)
-    c[-1] = -1
-    names = [f"({i},{j})" for i, j in zip(from_arc, to_arc)]
-    b = np.zeros(N_nodes)
-
-    # Solve flow model with gurobi
-    with gp.Env() as env, gp.Model(env=env) as m:
-        m.ModelSense = GRB.MINIMIZE
-        x = m.addMVar(s, lb=0, ub=u, obj=c, name=names)
-        m.addMConstr(A, x, GRB.EQUAL, b)
-        m.optimize()
-        flows = x.X
+    # Solve min-cost flow problem
+    with gp.Env() as env:
+        flows = solve_min_cost_flow(env, from_arc, to_arc, capacity, cost)
 
     # Choose the arcs corresponding to the original graph with non-zero
     # flow. Note that the last var is the sink->source connection (drop it).
@@ -113,7 +121,7 @@ def maximum_bipartite_matching_flow(adjacency, nodes1, nodes2):
     from_arc_result = from_arc[select][:-1]
     to_arc_result = to_arc[select][:-1]
 
-    logger.info(f"Maximum bipartite matching has {from_arc_result.shape[0]} edges")
+    logger.info(f"Done: max bipartite matching has {from_arc_result.shape[0]} edges")
 
     # Return undirected, unweighted adjacency matrix
     arg = (np.ones(from_arc_result.shape), (from_arc_result, to_arc_result))
