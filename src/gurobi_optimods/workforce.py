656e3588819b09f5acaa0386fbdda2c77fd7ba11
Simon Bowly <bowly@gurobi.com>
2023-04-28 15:51:07 +1000

    Convert workforce to preference model

--- src/gurobi_optimods/workforce.py
+++ src/gurobi_optimods/workforce.py
@@ -1,28 +1,11 @@
 import gurobipy as gp
-import numpy as np
+import gurobipy_pandas as gppd
 import pandas as pd
-import scipy.sparse as sp
-
-
-def workforce_mconstr(m, availability, shift_req, pay):
-
-    # Directly set the objective at construction time.
-    x = m.addMVar(availability.index.size, ub=1, obj=pay.loc[availability["Worker"]])
-
-    # Use pandas to munge data into matrix form.
-    categorical = availability["Shift"].astype("category")
-    row = categorical.cat.codes
-    col = availability.index
-    data = np.ones(availability.index.size)
-    A = sp.coo_array((data, (row, col)))
-    b = shift_req.loc[categorical.cat.categories].values
-    m.addMConstr(A, x, gp.GRB.EQUAL, b)
-
-    return x
+from gurobipy import GRB
 
 
 def solve_workforce_scheduling(
-    availability: pd.DataFrame, shift_requirements: pd.Series, pay_rates: pd.Series
+    availability: pd.DataFrame, shift_requirements: pd.DataFrame
 ) -> pd.DataFrame:
     """Solve a workforce scheduling model.
 
@@ -32,18 +15,23 @@ def solve_workforce_scheduling(
     :param shift_requirements: Dataframe with columns 'Shift' and 'Required'
         specifying the number of staff required for every shift
     :type shift_requirements: :class:`pd.DataFrame`
-    :param pay_rates: Dataframe with columns 'Worker' and 'PayRate' specifying
-        the per-shift pay rate of every worker
-    :type pay_rates: :class:`pd.DataFrame`
     """
     with gp.Env() as env, gp.Model(env=env) as m:
-        x = workforce_mconstr(
+        m.ModelSense = GRB.MAXIMIZE
+        assignments = availability.set_index(["Worker", "Shift"]).gppd.add_vars(
+            m, obj="Preference", vtype=GRB.BINARY, name="assign"
+        )
+        gppd.add_constrs(
             m,
-            availability,
+            assignments.groupby("Shift")["assign"].sum(),
+            GRB.EQUAL,
             shift_requirements.set_index("Shift")["Required"],
-            pay_rates.set_index("Worker")["PayRate"],
+            name="requirements",
         )
         m.optimize()
-        return availability[
-            pd.Series(index=availability.index, data=x.X > 0.9)
-        ].reset_index(drop=True)
+        return (
+            assignments.assign(assign=lambda df: df["assign"].gppd.X)
+            .query("assign > 0.9")
+            .drop(columns=["assign"])
+            .reset_index()
+        )
--- tests/test_workforce.py
+++ tests/test_workforce.py
@@ -1,9 +1,17 @@
 import unittest
+from io import StringIO
+from textwrap import dedent
 
+import pandas as pd
 from pandas.api.types import is_object_dtype, is_datetime64_any_dtype, is_numeric_dtype
+from pandas.testing import assert_frame_equal
 
 from gurobi_optimods.workforce import solve_workforce_scheduling
 from gurobi_optimods.datasets import load_workforce
+
+
+def read_csv(text):
+    return pd.read_csv(StringIO(dedent(text)))
 
 
 class TestWorkforceScheduling(unittest.TestCase):
@@ -24,3 +32,65 @@ class TestWorkforceScheduling(unittest.TestCase):
         self.assertEqual(set(data.shift_requirements.columns), {"Shift", "Required"})
         self.assertTrue(is_datetime64_any_dtype(data.shift_requirements["Shift"]))
         self.assertTrue(is_numeric_dtype(data.shift_requirements["Required"]))
+
+    def test_no_option(self):
+        # Simple example where there is only one way to cover requirements
+        availability = read_csv(
+            """
+            Worker,Shift,Preference
+            Bob,2022-07-02,1.0
+            Alice,2022-07-03,1.0
+            """
+        )
+        shift_requirements = read_csv(
+            """
+            Shift,Required
+            2022-07-02,1
+            2022-07-03,1
+            """
+        )
+
+        assignments = solve_workforce_scheduling(
+            availability=availability, shift_requirements=shift_requirements
+        )
+
+        self.assertIsInstance(assignments, pd.DataFrame)
+        self.assertIsNot(assignments, availability)
+        assert_frame_equal(assignments, availability)
+
+    def test_preferences(self):
+        # Choose an assignment which maximises preferences
+        availability = read_csv(
+            """
+            Worker,Shift,Preference
+            Alice,2022-07-02,1.0
+            Alice,2022-07-03,2.0
+            Bob,2022-07-02,2.0
+            Bob,2022-07-03,1.0
+            """
+        )
+        shift_requirements = read_csv(
+            """
+            Shift,Required
+            2022-07-02,1
+            2022-07-03,1
+            """
+        )
+
+        assignments = solve_workforce_scheduling(
+            availability=availability, shift_requirements=shift_requirements
+        )
+
+        expected = read_csv(
+            """
+            Worker,Shift,Preference
+            Alice,2022-07-03,2.0
+            Bob,2022-07-02,2.0
+            """
+        )
+        self.assertIsInstance(assignments, pd.DataFrame)
+        self.assertIsNot(assignments, availability)
+        assert_frame_equal(
+            assignments.sort_values(["Worker", "Shift"]).reset_index(drop=True),
+            expected,
+        )
