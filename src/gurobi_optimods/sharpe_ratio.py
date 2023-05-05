419f51141a9ad4e20827c9c2349ec2918eeb8590
etowle <8570492+etowle@users.noreply.github.com>
2023-05-04 20:48:59 -0500

    Implement Sharpe ratio mod

--- src/gurobi_optimods/sharpe_ratio.py
+++ src/gurobi_optimods/sharpe_ratio.py
@@ -1,22 +1,38 @@
-# Implementation of your new mod. This should be copied to
-# src/gurobi_optimods/<mod-name>.py. You may alternatively want to include
-# your mod in an existing file, if it coexists naturally with other mods.
-#
-# In general the public API should be a single class or function. Go with
-# whatever makes the most sense for this mod.
-
 import gurobipy as gp
+from gurobipy import GRB
+import numpy as np
+import math
+
+from gurobi_optimods.utils import optimod
 
 
-def solve_mod(data):
+@optimod()
+def max_sharpe_ratio(Q: np.ndarray, mu: np.ndarray, rf_rate: float, *, create_env):
     """
-    A sphinx-compatible docstring
+    Solve the problem of finding a portfolio that maximizes the
+    Sharpe ratio.
 
-    :param data1: Data structure for first argument
-    :type data1: pd.DataFrame
+    :param Q: 2D positive-semidefinite variance-covariance matrix.
+    :type Q: :class:`np.ndarray `
+    :param mu: Return rates.
+    :type mu: :class:`np.ndarray`
+    :param rf_rate: Risk-free rate of return.
+    :type rf_rate: float
+    :return: Portfolio that maximizes the Sharpe ratio.
+    :rtype: :class:`np.ndarray`
+    :param silent: Optional. Boolean with whether output should be printed.
+    :type silent: :class:`bool`
+    :param logfile: Optional. String with file path with logger and Gurobi.
+    :type logfile: :class:`str`
+    :return: Sharpe ratio of the portfolio.
+    :rtype: float
     """
-    with gp.Env() as env, gp.Model(env=env) as model:
-        # build model
+    with create_env() as env, gp.Model("sharpe_ratio", env=env) as model:
+        y = model.addMVar(mu.size, name="y")
+        model.addConstr((mu - rf_rate) @ y == 1)
+        model.setObjective(y @ Q @ y, sense=GRB.MINIMIZE)
+
         model.optimize()
-        # post-process and return solution
-        return
+
+        # Translate solution to original variable space before returning
+        return y.X / y.X.sum(), 1 / math.sqrt(model.ObjVal)
