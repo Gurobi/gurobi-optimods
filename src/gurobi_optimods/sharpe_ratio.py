17b4546c7f5918c99d4eaeb2be50c909af35b1c4
etowle <8570492+etowle@users.noreply.github.com>
2023-05-23 19:24:25 -0500

    Add pandas interface

--- src/gurobi_optimods/sharpe_ratio.py
+++ src/gurobi_optimods/sharpe_ratio.py
@@ -1,32 +1,61 @@
 import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
+import pandas as pd
 import math
 
 from gurobi_optimods.utils import optimod
 
 
 @optimod()
-def max_sharpe_ratio(Q: np.ndarray, mu: np.ndarray, rf_rate: float, *, create_env):
+def max_sharpe_ratio(Q, mu, rf_rate, *, create_env):
     """
     Solve the problem of finding a portfolio that maximizes the
     Sharpe ratio.
 
     :param Q: 2D positive-semidefinite variance-covariance matrix.
-    :type Q: :class:`np.ndarray `
+    :type Q: :class:`np.ndarray|pd.DataFrame`
     :param mu: Return rates.
-    :type mu: :class:`np.ndarray`
+    :type mu: :class:`np.ndarray|pd.Series`
     :param rf_rate: Risk-free rate of return.
     :type rf_rate: float
     :return: Portfolio that maximizes the Sharpe ratio.
-    :rtype: :class:`np.ndarray`
+    :rtype: :class:`np.ndarray|pd.Series`
     :param silent: Optional. Boolean with whether output should be printed.
     :type silent: :class:`bool`
     :param logfile: Optional. String with file path with logger and Gurobi.
     :type logfile: :class:`str`
+    :return: Optimal portfolio.
+    :rtype: :class:`np.ndarray|pd.Series`
     :return: Sharpe ratio of the portfolio.
     :rtype: float
     """
+    indices = None
+
+    if isinstance(Q, pd.DataFrame):
+        indices = Q.index
+        Q = Q.to_numpy()
+    elif not isinstance(Q, np.ndarray):
+        raise ValueError(f"Unknown covariance matrix type: {type(Q)}")
+
+    if isinstance(mu, pd.Series):
+        if indices is None:
+            indices = mu.index
+        elif not mu.index.equals(indices):
+            raise ValueError("Indices of Q and mu are misaligned")
+        mu = mu.to_numpy()
+    elif not isinstance(mu, np.ndarray):
+        raise ValueError(f"Unknown return rates type: {type(mu)}")
+
+    portfolio, ratio = _max_sharpe_ratio_numpy(Q, mu, rf_rate, create_env)
+
+    if indices is not None:
+        portfolio = pd.Series(data=portfolio, index=indices)
+
+    return portfolio, ratio
+
+
+def _max_sharpe_ratio_numpy(Q, mu, rf_rate, create_env):
     with create_env() as env, gp.Model("sharpe_ratio", env=env) as model:
         y = model.addMVar(mu.size, name="y")
         model.addConstr((mu - rf_rate) @ y == 1)
