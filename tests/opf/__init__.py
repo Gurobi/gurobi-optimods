9fc75060563952c32419cfb6b03e43b94c3733dd
Simon Bowly <bowly@gurobi.com>
2023-07-19 01:17:46 +1000

    Adapt input format

--- tests/opf/__init__.py
+++ tests/opf/__init__.py
@@ -4,9 +4,6 @@ def read_case(case):
 
     file_path = load_caseopfmat(case)
     case_data = read_case_matfile(file_path)
-    for key in ["bus", "gen", "branch", "gencost"]:
-        case_data[key] = {i + 1: entry for i, entry in enumerate(case_data[key])}
-
     return case_data
 
 
@@ -15,51 +12,55 @@ def load_case9branchswitching():
     # create an artifical case, where turning off 2 branches
     # produces a better solution
     casefile_dict = read_case("9")
-    casefile_dict["branch"][10] = {
-        "fbus": 1,
-        "tbus": 2,
-        "r": 0.0,
-        "x": 0.0576,
-        "b": 0.0,
-        "rateA": 250.0,
-        "rateB": 250.0,
-        "rateC": 250.0,
-        "ratio": 1.0,
-        "angle": 0.0,
-        "status": 1,
-        "angmin": -360.0,
-        "angmax": 360.0,
-    }
-    casefile_dict["branch"][11] = {
-        "fbus": 1,
-        "tbus": 3,
-        "r": 0.0,
-        "x": 0.0576,
-        "b": 0.0,
-        "rateA": 250.0,
-        "rateB": 250.0,
-        "rateC": 250.0,
-        "ratio": 1.0,
-        "angle": 0.0,
-        "status": 1,
-        "angmin": -360.0,
-        "angmax": 360.0,
-    }
-    casefile_dict["branch"][12] = {
-        "fbus": 2,
-        "tbus": 3,
-        "r": 0.0,
-        "x": 0.0576,
-        "b": 0.0,
-        "rateA": 250.0,
-        "rateB": 250.0,
-        "rateC": 250.0,
-        "ratio": 1.0,
-        "angle": 0.0,
-        "status": 1,
-        "angmin": -360.0,
-        "angmax": 360.0,
-    }
+    casefile_dict["branch"].extend(
+        [
+            {
+                "fbus": 1,
+                "tbus": 2,
+                "r": 0.0,
+                "x": 0.0576,
+                "b": 0.0,
+                "rateA": 250.0,
+                "rateB": 250.0,
+                "rateC": 250.0,
+                "ratio": 1.0,
+                "angle": 0.0,
+                "status": 1,
+                "angmin": -360.0,
+                "angmax": 360.0,
+            },
+            {
+                "fbus": 1,
+                "tbus": 3,
+                "r": 0.0,
+                "x": 0.0576,
+                "b": 0.0,
+                "rateA": 250.0,
+                "rateB": 250.0,
+                "rateC": 250.0,
+                "ratio": 1.0,
+                "angle": 0.0,
+                "status": 1,
+                "angmin": -360.0,
+                "angmax": 360.0,
+            },
+            {
+                "fbus": 2,
+                "tbus": 3,
+                "r": 0.0,
+                "x": 0.0576,
+                "b": 0.0,
+                "rateA": 250.0,
+                "rateB": 250.0,
+                "rateC": 250.0,
+                "ratio": 1.0,
+                "angle": 0.0,
+                "status": 1,
+                "angmin": -360.0,
+                "angmax": 360.0,
+            },
+        ]
+    )
     casefile_dict["gencost"][2]["costvector"] = [0.85, 10.2, 1200]
 
     return casefile_dict
