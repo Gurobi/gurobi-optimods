fedcfc9e3fd7d5c69acbcb3fd8b105d6cc98e25a
Simon Bowly <bowly@gurobi.com>
2023-07-19 10:57:30 +1000

    Adjust voltages test

--- tests/opf/test_voltages.py
+++ tests/opf/test_voltages.py
@@ -2,25 +2,27 @@
 
 import unittest
 
-from gurobi_optimods.datasets import load_filepath, load_opfdictcase
-from gurobi_optimods.opf import (
-    compute_violations_from_given_voltages,
-    read_voltages_from_csv_file,
-)
+from gurobi_optimods.opf import compute_violations_from_given_voltages
 
 
 class TestComputeVoltages(unittest.TestCase):
     # test violation computation out of pre-defined voltage data
+
+    def setUp(self):
+        from gurobi_optimods.datasets import load_filepath
+        from gurobi_optimods.opf import read_voltages_from_csv_file
+        from tests.opf import read_case
+
+        self.case = read_case("9")
+        self.volts_data = read_voltages_from_csv_file(load_filepath("case9volts.csv"))
+
     def test_volts(self):
-        # get path to csv file holding the input voltages for case 9
-        voltsfile = load_filepath("case9volts.csv")
-        volts_dict = read_voltages_from_csv_file(voltsfile)
-        # load case dictionary
-        case = load_opfdictcase()
-        # compute violations
-        violations = compute_violations_from_given_voltages(case, volts_dict, True)
+        violations = compute_violations_from_given_voltages(
+            self.case, self.volts_data, polar=True
+        )
+
         self.assertTrue(violations is not None)
-        self.assertLess(abs(violations["bus"][2]["Vmviol"]), 1e-9)
-        self.assertLess(abs(violations["bus"][4]["Pviol"] + 318.899783), 1e-4)
-        self.assertLess(abs(violations["bus"][7]["Qviol"] - 9.8410206), 1e-4)
-        self.assertLess(abs(violations["branch"][7]["limitviol"] - 66.33435), 1e-4)
+        self.assertLess(abs(violations["bus"][1]["Vmviol"]), 1e-9)
+        self.assertLess(abs(violations["bus"][3]["Pviol"] + 318.899783), 1e-4)
+        self.assertLess(abs(violations["bus"][6]["Qviol"] - 9.8410206), 1e-4)
+        self.assertLess(abs(violations["branch"][6]["limitviol"] - 66.33435), 1e-4)
