22f00f204d45ecfc3e57da94f887fc4d2da10557
Simon Bowly <bowly@gurobi.com>
2023-07-19 12:28:06 +1000

    load cases directly in datasets

--- tests/opf/test_io.py
+++ tests/opf/test_io.py
@@ -5,17 +5,31 @@ import pathlib
 import tempfile
 import unittest
 
+from gurobi_optimods.datasets import load_opf_example
 from gurobi_optimods.opf.io import read_case_matfile, write_case_matfile
 
 
+class TestDatasets(unittest.TestCase):
+    def test_load_opf_example(self):
+        # Check that example cases can be loaded by name
+        for case in ["case9", "case14", "case57", "case118", "case300", "caseNY"]:
+            case_data = load_opf_example(case)
+            self.assertEqual(
+                set(case_data.keys()), {"baseMVA", "bus", "branch", "gen", "gencost"}
+            )
+
+
 class TestIO(unittest.TestCase):
-    def test_roundtrip(self):
+    def setUp(self):
+        here = pathlib.Path(__file__).parent.resolve()
+        self.testdata_dir = here.joinpath("data")
+        self.dataset_dir = here.parent.parent.joinpath("src/gurobi_optimods/data/opf")
+
+    def test_matfile_roundtrip(self):
         # round trip test of dictionary format
 
         original = json.loads(
-            pathlib.Path(__file__)
-            .parent.joinpath("data/bus2-branch2-gen2.json")
-            .read_text()
+            self.testdata_dir.joinpath("bus2-branch2-gen2.json").read_text()
         )
 
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -38,13 +52,12 @@ class TestIO(unittest.TestCase):
             self.assertIsInstance(branch["fbus"], int)
             self.assertIsInstance(branch["tbus"], int)
 
-    def test_read_case(self):
+    def test_read_case_matfile(self):
         # Check that all example cases are read without errors
 
-        from gurobi_optimods.datasets import load_caseopfmat
-
         case_mat_files = [
-            load_caseopfmat(case) for case in ["9", "14", "57", "118", "300", "NY"]
+            self.dataset_dir.joinpath(f"case{case}.mat")
+            for case in ["9", "14", "57", "118", "300", "NY"]
         ]
 
         for file_path in case_mat_files:
