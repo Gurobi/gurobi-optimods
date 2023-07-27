2106218645a1c35f8c6b09cda47c1cc2ef9d85fa
Simon Bowly <bowly@gurobi.com>
2023-07-27 17:12:03 +1000

    Refactor DC model

--- src/gurobi_optimods/opf/grbformulator_dc.py
+++ src/gurobi_optimods/opf/grbformulator_dc.py
@@ -11,44 +11,35 @@ logger = logging.getLogger(__name__)
 
 
 def lpformulator_dc_body(alldata, model):
-    """
-    Adds variables and constraints for DC formulation to a given Gurobi model
+    """Builds the DCOPF formulation in the given model"""
 
-    :param alldata: Main dictionary holding all necessary data
-    :type alldata: dict
-    :param model: Gurobi model to be constructed
-    :type model: :class: `gurobipy.Model`
-    """
-
-    lpformulator_dc_create_vars(alldata, model)
+    _add_dc_gen_bus_variables(alldata, model)
+    _add_dc_branch_variables(alldata, model)
     set_gencost_objective(alldata, model)
-    lpformulator_dc_create_constraints(alldata, model)
+    _add_dc_branch_activepower_constraints(alldata, model)
+    _add_dc_bus_balance_constraints(alldata, model)
+    _add_dc_bus_injection_constraints(alldata, model)
 
 
-def lpformulator_dc_create_vars(alldata, model):
+def _add_dc_gen_bus_variables(alldata, model):
     """
-    Creates and adds variables for DC formulation to a given Gurobi model
+    Add variables associated with buses and generators
 
-    :param alldata: Main dictionary holding all necessary data
-    :type alldata: dict
-    :param model: Gurobi model to be constructed
-    :type model: :class: `gurobipy.Model`
+    - thetavar: voltage angle (voltage magnitude is always 1 for DC)
+    - Pinjvar: variable modeling total active power injected by bus j into the
+      branches incident with j
+    - GenPVar: DC generator real power injection variables
     """
-
-    logger.info("Creating variables.")
 
     fixtolerance = alldata["fixtolerance"]
 
     numbuses = alldata["numbuses"]
     buses = alldata["buses"]
-    IDtoCountmap = alldata["IDtoCountmap"]
     gens = alldata["gens"]
 
     thetavar = {}
     Pinjvar = {}
-    Pvar_f = {}  # DC, so f-flow = - t-flow
-    twinPvar_f = {}  # auxiliary variable in case branch-switching is being used
-    GenPvar = {}  # DC, generator real power injections variables
+    GenPvar = {}
 
     for j in range(1, numbuses + 1):
         bus = buses[j]
@@ -65,26 +56,48 @@ def lpformulator_dc_create_vars(alldata, model):
             lb=lbound, ub=ubound, name="theta_" + str(bus.nodeID)
         )
 
-        Plbound = Qlbound = -GRB.INFINITY
-        Pubound = Qubound = GRB.INFINITY
-        Pubound, Plbound, Qubound, Qlbound = computebalbounds(alldata, bus)
+        Pubound, Plbound, _, _ = computebalbounds(alldata, bus)
 
         Pinjvar[bus] = model.addVar(lb=Plbound, ub=Pubound, name="IP_%d" % bus.nodeID)
-        # comment: Pinjvar is the variable modeling total active power injected by bus j into the branches incident with j
 
-        # Next, generator variables
+        # This is done in the inner loop because each generator should have only
+        # one associated bus. However it would be cleaner to just iterate over
+        # the generators.
         for genid in bus.genidsbycount:
             gen = gens[genid]
             lower = gen.Pmin * gen.status
             upper = gen.Pmax * gen.status
             # if bus.nodetype == 3:
-            #  upper = GRB.INFINITY
-            #  lower = -GRB.INFINITY  #ignoring slack bus
+            #     upper = GRB.INFINITY
+            #     lower = -GRB.INFINITY  #ignoring slack bus
             GenPvar[gen] = model.addVar(
                 lb=lower, ub=upper, name="GP_%d_%d" % (gen.count, gen.nodeID)
             )
 
-    # Branch related variables
+    alldata["LP"]["thetavar"] = thetavar
+    alldata["LP"]["Pinjvar"] = Pinjvar
+    alldata["LP"]["GenPvar"] = GenPvar
+
+
+def _add_dc_branch_variables(alldata, model):
+    """
+    Add variables associated with branches
+
+    Always:
+        - Pvar_f: DC branch real power injected into "from" end of branch. DC
+          branch real power injected into "to" end of branch is the same as
+          Pvar_f.
+
+    If branch switching is used:
+        - twinPvar_f: Auxiliary variable in case branch-switching is being used
+        - zvar: branch switching decision variable
+    """
+
+    buses = alldata["buses"]
+    IDtoCountmap = alldata["IDtoCountmap"]
+    Pvar_f = {}  # DC, so f-flow = - t-flow
+    twinPvar_f = {}
+
     branches = alldata["branches"]
     numbranches = alldata["numbranches"]
 
@@ -117,7 +130,6 @@ def lpformulator_dc_create_vars(alldata, model):
 
     zvar = {}
     if alldata["branchswitching_mip"]:
-        logger.info("Adding branch switching variables.")
         for j in range(1, 1 + numbranches):
             branch = branches[j]
             f = branch.f
@@ -128,36 +140,20 @@ def lpformulator_dc_create_vars(alldata, model):
                 name="z_%d_%d_%d" % (j, f, t),
             )
 
-    alldata["LP"][
-        "thetavar"
-    ] = thetavar  # for DC this is the voltage angle - voltage magnitude is always 1 for DC
-    alldata["LP"]["Pinjvar"] = Pinjvar
-    alldata["LP"]["GenPvar"] = GenPvar  # DC, generator real power injections variables
-    alldata["LP"][
-        "Pvar_f"
-    ] = Pvar_f  # DC branch real power injected into "from" end of branch
-    # DC branch real power injected into "to" end of branch is the same as Pvar_f
+    alldata["LP"]["Pvar_f"] = Pvar_f
+
     if alldata["branchswitching_mip"]:
         alldata["LP"]["twinPvar_f"] = twinPvar_f
 
     alldata["MIP"]["zvar"] = zvar
 
 
-def lpformulator_dc_create_constraints(alldata, model):
-    """
-    Creates and adds constraints for DC formulation to a given Gurobi model
+def _add_dc_branch_activepower_constraints(alldata, model):
+    """Add constraints defining active power"""
 
-    :param alldata: Main dictionary holding all necessary data
-    :type alldata: dict
-    :param model: Gurobi model to be constructed
-    :type model: :class: `gurobipy.Model`
-    """
-
-    numbuses = alldata["numbuses"]
     buses = alldata["buses"]
     numbranches = alldata["numbranches"]
     branches = alldata["branches"]
-    gens = alldata["gens"]
     IDtoCountmap = alldata["IDtoCountmap"]
 
     thetavar = alldata["LP"]["thetavar"]
@@ -165,11 +161,7 @@ def lpformulator_dc_create_constraints(alldata, model):
     if alldata["branchswitching_mip"]:
         twinPvar_f = alldata["LP"]["twinPvar_f"]
     zvar = alldata["MIP"]["zvar"]
-    Pinjvar = alldata["LP"]["Pinjvar"]
-    GenPvar = alldata["LP"]["GenPvar"]
 
-    # Active PF defs
-    logger.info("  Adding active power flow definitions.")
     count = 0
     for j in range(1, 1 + numbranches):
         branch = branches[j]
@@ -225,13 +217,16 @@ def lpformulator_dc_create_constraints(alldata, model):
                 name="dnmip_twin_%d_%d_%d" % (j, f, t),
             )
 
-    logger.info(f"    {count} active power flow definitions added.")
 
-    # Balance constraints
-    logger.info(
-        "  Adding constraints stating bus injection = total outgoing power flow."
-    )
-    count = 0
+def _add_dc_bus_balance_constraints(alldata, model):
+    """Balance definintions"""
+
+    numbuses = alldata["numbuses"]
+    buses = alldata["buses"]
+    branches = alldata["branches"]
+    Pvar_f = alldata["LP"]["Pvar_f"]
+    Pinjvar = alldata["LP"]["Pinjvar"]
+
     balancecons = {}
     for j in range(1, 1 + numbuses):
         bus = buses[j]
@@ -241,18 +236,27 @@ def lpformulator_dc_create_constraints(alldata, model):
 
         for branchid in bus.tobranchids.values():
             expr.add(-Pvar_f[branches[branchid]])
-        # Create dictionary accessed by bus holding these constraints to access their duals afterwards
+        # Create dictionary accessed by bus holding these constraints to
+        # access their duals afterwards
         balancecons[bus] = model.addConstr(
             expr == Pinjvar[bus], name="PBaldef%d_%d" % (j, bus.nodeID)
         )
 
-        count += 1
     alldata["LP"]["balancecons"] = balancecons
-    logger.info(f"    {count} constraints added.")
 
-    # Injection defs
-    logger.info("  Adding injection definition constraints.")
-    count = 0
+
+def _add_dc_bus_injection_constraints(alldata, model):
+    """Injection definitions"""
+
+    numbuses = alldata["numbuses"]
+    buses = alldata["buses"]
+    numbranches = alldata["numbranches"]
+    branches = alldata["branches"]
+    gens = alldata["gens"]
+    zvar = alldata["MIP"]["zvar"]
+    Pinjvar = alldata["LP"]["Pinjvar"]
+    GenPvar = alldata["LP"]["GenPvar"]
+
     for j in range(1, 1 + numbuses):
         bus = buses[j]
         expr = gp.LinExpr()
@@ -263,9 +267,6 @@ def lpformulator_dc_create_constraints(alldata, model):
                 expr.add(GenPvar[gen])
 
         model.addConstr(Pinjvar[bus] == expr - bus.Pd, name="Bus_PInj_%d" % j)
-        count += 1
-
-    logger.info(f"    {count} injection definition constraints added.")
 
     if alldata["branchswitching_mip"]:
         expr = gp.LinExpr()
