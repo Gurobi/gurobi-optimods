442162bd17e4ef84cafa46a959b9a1d1d604aca8
Mario Ruthmair <ruthmair@gurobi.com>
2023-09-25 18:15:33 +0200

    classical SP without resource constraints works

--- tests/test_shortest_path.py
+++ tests/test_shortest_path.py
@@ -7,17 +7,17 @@ import scipy.sparse as sp
 import gurobi_optimods.shortest_path as sp
 
 node_data_csv = """
-node,cost,time
-0,0,1
-1,0,1
-2,0,1
-3,0,1
-4,0,1
-5,0,1
+i,cost,time
+0,1,5
+1,1,4
+2,1,3
+3,1,2
+4,1,1
+5,1,0
 """
 
 arc_data_csv = """
-from,to,cost
+i,j,cost
 0,1,1
 0,2,3
 1,2,-5
@@ -33,8 +33,8 @@ from,to,cost
 
 def load_graph_pandas():
     return (
-        pd.read_csv(io.StringIO(node_data_csv)).set_index(["node"]),
-        pd.read_csv(io.StringIO(arc_data_csv)).set_index(["from", "to"]),
+        pd.read_csv(io.StringIO(node_data_csv)).set_index(["i"]),
+        pd.read_csv(io.StringIO(arc_data_csv)).set_index(["i", "j"]),
     )
 
 
@@ -42,7 +42,7 @@ class TestShortestPath(unittest.TestCase):
     def test_negative_costs(self):
         node_data, arc_data = load_graph_pandas()
         path = sp.shortest_path(node_data, arc_data, 0, 5)
-        self.assertEqual(path.cost, -2)
+        self.assertEqual(path.cost, 4)
         arc_list = [0, 1, 2, 3, 4, 5]
         self.assertIsInstance(path.nodes, list)
         self.assertEqual(path.nodes, arc_list)
