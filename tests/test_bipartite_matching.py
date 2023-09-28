b3991452489888c73e68167c6ffcc962da238b4f
torressa <23246013+torressa@users.noreply.github.com>
2023-09-28 14:19:09 +0100

    Change csc/csc_matrix to array

--- tests/test_bipartite_matching.py
+++ tests/test_bipartite_matching.py
@@ -126,11 +126,11 @@ class TestBipartiteMatchingScipySparse(unittest.TestCase):
         self.assertEqual(matching.shape, adjacency.shape)
         self.assert_is_unweighted_matching(matching)
 
-    def test_random_csr_matrix(self):
+    def test_random_csr_array(self):
         # Property test for matchings on random graphs
         adjacency, nodes1, nodes2 = random_bipartite(n1=8, n2=7, p=0.5, seed=98634)
 
-        matching = maximum_bipartite_matching(sp.csr_matrix(adjacency), nodes1, nodes2)
+        matching = maximum_bipartite_matching(sp.csr_array(adjacency), nodes1, nodes2)
 
         self.assertIsInstance(matching, sp.spmatrix)
         self.assertIsNot(matching, adjacency)
